e58ed96403bb166886fea11f6c488830c7c3fb1d
PrathamPatel2004 <137031619+PrathamPatel2004@users.noreply.github.com>
2025-09-20 16:11:26 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ credentials : true, origin : process.env.FRONTEND_URL }));
+app.use(cors({ credentials : true, origin : 'https://character-hub-nine.vercel.app' }));
 app.use(cookieParser())
 app.use(express.json());
 
