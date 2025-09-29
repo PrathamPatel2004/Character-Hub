7704190b113f915e864490a52999213d4a361769
PrathamPatel2004 <137031619+PrathamPatel2004@users.noreply.github.com>
2025-09-29 08:26:43 +0530

    Update Login.jsx

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            const res = await fetch(`${import.meta.env.REACT_APP_API_BASE_URL}/api/auth/login`, {
+            const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 credentials: 'include',
