804113ede0f203ff6a89375991d5d60b9286605b
PrathamPatel2004 <137031619+PrathamPatel2004@users.noreply.github.com>
2025-09-29 06:20:32 +0530

    Update auth.js

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { allUsers, registerUser, OTPVerification, ResendOTP, LoginUser, ForgotPassword, getProfileInfo, addProfileInfo, verifyAccessToken, logoutUser, followUser, unFollowUser, checkFollowStatus } from '../Controllers/userController.js'
+import { allUsers, registerUser, OTPVerification, ResendOTP, LoginUser, ForgotPassword, resetPassword, changePassword, validateResetToken, getProfileInfo, addProfileInfo, verifyAccessToken, logoutUser, followUser, unFollowUser, checkFollowStatus } from '../Controllers/userController.js'
 import upload from '../MiddleWare/Multer.js'
 import auth from '../MiddleWare/auth.js'
 
@@ -10,7 +10,10 @@ userRouter.post('/register', registerUser)
 userRouter.post('/verify-OTP', OTPVerification)
 userRouter.post('/login', LoginUser)
 userRouter.post('/resend-otp', ResendOTP)
-userRouter.put('/forgot-password', ForgotPassword)
+userRouter.post('/forgot-password', ForgotPassword)
+userRouter.post('/reset-password/:token', resetPassword)
+userRouter.get('/validate-reset-token/:token', validateResetToken)
+userRouter.put('/change-password', auth, changePassword)
 userRouter.get('/get-user-info/:id', auth, getProfileInfo)
 userRouter.put('/add-profile-info', auth, upload.single('image'), addProfileInfo);
 userRouter.get('/verify-access-token', verifyAccessToken);
