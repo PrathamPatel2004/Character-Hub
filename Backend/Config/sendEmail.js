e7056044f2637c83e1f4af62b1b3fb1ec6689c86
PrathamPatel2004 <137031619+PrathamPatel2004@users.noreply.github.com>
2025-10-05 01:32:00 +0530

    Update sendEmail.js

--- Backend/Config/sendEmail.js
+++ Backend/Config/sendEmail.js
@@ -1,32 +1,32 @@
 import nodemailer from 'nodemailer';
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service : 'gmail',
-    auth : {
-        user : process.env.EMAIL_USER,
-        pass : process.env.EMAIL_PASS
-    }
+  service: "SendGrid",
+  auth: {
+    user: "apikey", // this is literally the string "apikey"
+    pass: process.env.SENDGRID_API_KEY,
+  },
 });
 
 const sendEmail = async ({ to, subject, text, html }) => {
-    const mailOptions = {
-        from : `"Character Wiki" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        text,
-        html
-    };
+  const mailOptions = {
+    from: `"Character Wiki" <no-reply@yourdomain.com>`, // use a verified sender
+    to,
+    subject,
+    text,
+    html,
+  };
 
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent : ', info.response);
-        return info;
-    } catch (error) {
-        console.error('Error sending email : ', error);
-        throw error;
-    }
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
 };
 
-export default sendEmail
+export default sendEmail;
