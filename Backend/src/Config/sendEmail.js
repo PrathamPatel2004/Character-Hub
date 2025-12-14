50db0c7f1020cc0a7d8a0dbbfa04b526dad12562
PrathamPatel2004 <137031619+PrathamPatel2004@users.noreply.github.com>
2025-12-14 17:35:50 +0530

    sendEmail.js changes

--- Backend/src/Config/sendEmail.js
+++ Backend/src/Config/sendEmail.js
@@ -2,33 +2,29 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-  },
-});
-
-
 const sendEmail = async ({ to, subject, text, html }) => {
-    try {
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to,
-            subject,
-            text,
-            html,
-        };
 
-        const result = await transporter.sendMail(mailOptions);
-
-        console.log("Email sent:", result.messageId);
-        return result;
-    } catch (error) {
-        console.error("Error sending email:", error);
-        throw error;
+    if (process.env.NODE_ENV === "production") {
+        console.log("Email skipped (SMTP blocked on Vercel)");
+        return;
     }
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        text,
+        html,
+    });
+
 };
 
 export default sendEmail;
